1d4c3d3b296dcbf2d804f744377d3d48ca6821bf
D.F <a2470abc@gmail.com>
2019-03-26 02:05:12 +0800

    modueling done
    TODO:
    make multi-level directory

--- PythonCrawlerForKaobeiEngineer.py
+++ PythonCrawlerForKaobeiEngineer.py
@@ -1,9 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
-
+import os
 def htmlPaserAndDownloader(CategoryTag,pathFix):
 
-    url = 'https://kaobei.engineer/rankings/+'+ CategoryTag
+    url = 'https://kaobei.engineer/rankings/'+ CategoryTag
     tag = '.col-block a img'
     result = requests.get(url)
     soup = BeautifulSoup(result.text, 'html.parser')
@@ -12,10 +12,13 @@ def htmlPaserAndDownloader(CategoryTag,pathFix):
     for img in soup.select(tag):
         index += 1
         img_data = requests.get(img.get('src')).content
-        with open('./data/'+pathFix+'/'+pathFix+'_{}.jpg'.format(index), 'wb') as handler:
-            handler.write(img_data)
 
-print('done')
+        if not os.path.isdir(pathFix):
+            os.mkdir(pathFix)
+        with open( pathFix + '/' + 'top_{}.jpg'.format(index), 'wb') as handler:
+            handler.write(img_data)
+    print('file downloaded')
+    print('done')
 
 if __name__ == '__main__':
     print("Execute locally")
--- mainPage.py
+++ mainPage.py
@@ -1,30 +1,34 @@
 import PythonCrawlerForKaobeiEngineer as ps
 
-
 def main(): 
     categoryStr = ['','monthly','quarterly','top2018']
                 # weektop , monthluTop, seasonTop, yearTop
-    
+                #  
     messageStr  = ['1.周排名','2.月排名','3.季排名','4.2018排名']
     
     print("欲爬下來的圖片分類 請輸入數字")
+
     for i in range(4):
         print(messageStr[i])
-    selection =  input()
-    intSelection = int(selection)
 
-    while intSelection >=1 and intSelection <= 4:
-        selection =  input()
-        intSelection = int(selection)
-        ps.htmlPaserAndDownloader(categoryStr[ intSelection ],categoryStr[intSelection]+"top")
-    else:
+    selection =  int(input()) - 1
+    while (selection > 3) or (selection < 0):
+        #輸入錯誤部分
+        # 輸入跟index是不同的
+        
         print("輸入錯誤，請重新輸入")
-        print("欲爬下來的圖片分類 請輸入數字")
         for i in range(4):
             print(messageStr[i])
+        selection =  int(input()) -1
+    else:
+        ps.htmlPaserAndDownloader(categoryStr[ selection ], categoryStr[selection]+"top")
+        print("call func")
 
 
 print('done')
 
+
+
 if __name__ == '__main__':
     main()  # 或是任何你想執行的函式
+    print('execute localy module')
